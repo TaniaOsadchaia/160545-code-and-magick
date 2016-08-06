fd5bf37173c196fb5db9f5df505a276fe923bc4c
TaniaOsadchaia <osatanchik@gmail.com>
2016-08-06 15:36:12 +0300

    Переписали check.js

--- build/js/check.js
+++ build/js/check.js
@@ -1,43 +1,33 @@
 function getMessage(a, b) {
-	var result;
 	var typeA = typeof a;
-	switch (typeA) {
 
-		case 'boolean':
-
-			if (a) {
-				result = 'Я попал в ' + b;
-			} else {
-				result = 'Я никуда не попал';
-			}
-			break;
-
-		case 'number':
-
-			result = 'Я прыгнул на ' + (a * 100) + ' сантиметров';
-			break;
-
-		case 'object':
-
-			if (a instanceof Array) {
-				if (b instanceof Array) {
-					var distancePath = 0;
-					for (var i = 0; i < a.length; i++) {
-						distancePath += (a[i] * b[i]);
-					}
-					result = 'Я прошёл ' + distancePath + ' метров';
-				} else {
-					var numberOfSteps = 0;
-					for (var i = 0; i < a.length; i++) {
-						numberOfSteps += a[i];
-					}
-					result = 'Я прошёл ' + numberOfSteps + ' шагов';
-				}
-				break;
-			}
-
-		default:
-			result = 'unknown type';
+	if (typeA == 'number') {
+		return 'Я прыгнул на ' + (a * 100) + ' сантиметров';
 	}
-	return result;
+
+	if (typeA == 'boolean') {
+		if (a) {
+			return 'Я попал в ' + b;
+		} else {
+			return 'Я никуда не попал';
+		}
+	}
+
+	if (a instanceof Array) {
+		if (b instanceof Array) {
+			var distancePath = 0;
+			for (var i = 0; i < a.length; i++) {
+				distancePath += (a[i] * b[i]);
+			}
+			return 'Я прошёл ' + distancePath + ' метров';
+		} else {
+			var numberOfSteps = 0;
+			for (var i = 0; i < a.length; i++) {
+				numberOfSteps += a[i];
+			}
+			return 'Я прошёл ' + numberOfSteps + ' шагов';
+		}
+	}
+
+	return 'bad params';
 }
