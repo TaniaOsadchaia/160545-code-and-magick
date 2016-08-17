f7fd1b2f81a1c71eda26c6294d817d5fcea1a449
TaniaOsadchaia <osatanchik@gmail.com>
2016-08-18 00:38:59 +0300

    Проверяем на валидацию имя и отзыв

--- build/js/main.js
+++ build/js/main.js
@@ -81,6 +81,12 @@
 	window.form = (function() {
 	  var formContainer = document.querySelector('.overlay-container');
 	  var formCloseButton = document.querySelector('.review-form-close');
+	  var formSubmitButton = document.querySelector('.review-submit');
+	  var formNameInput = document.querySelector('.review-form-field-name');
+	  var formNameReminder = document.querySelector('.review-fields-name');
+	  var formTextInput = document.querySelector('.review-form-field-text');
+	  var formTextReminder = document.querySelector('.review-fields-text');
+	  var formStars = document.querySelectorAll('.review-mark-label');
 	
 	  var form = {
 	    onClose: null,
@@ -90,6 +96,7 @@
 	     */
 	    open: function(cb) {
 	      formContainer.classList.remove('invisible');
+	      this.checkValidation();
 	      cb();
 	    },
 	
@@ -99,6 +106,37 @@
 	      if (typeof this.onClose === 'function') {
 	        this.onClose();
 	      }
+	    },
+	
+	    checkValidation: function() {
+	      var isValidName = this.checkValidationName();
+	      var isValidText = this.checkValidationText(this.getNumStars());
+	      var isValid = isValidName && isValidText;
+	      return isValid;
+	    },
+	
+	    checkValidationName: function() {
+	      var isValid = (formNameInput.value !== '');
+	      this.setVisible(formNameReminder, !isValid);
+	      return isValid;
+	    },
+	
+	    checkValidationText: function(numStars) {
+	      var isValid = (numStars >= 3 || formTextInput.value !== '');
+	      this.setVisible(formTextReminder, !isValid);
+	      return isValid;
+	    },
+	
+	    setVisible: function(obj, value) {
+	      if (value) {
+	        obj.classList.remove('invisible');
+	      } else {
+	        obj.classList.add('invisible');
+	      }
+	    },
+	
+	    getNumStars: function() {
+	      return document.querySelector('input[name="review-mark"]:checked').value;
 	    }
 	  };
 	
@@ -107,6 +145,30 @@
 	    evt.preventDefault();
 	    form.close();
 	  };
+	
+	  formSubmitButton.onclick = function(evt) {
+	    var isValid = form.checkValidation();
+	    if (!isValid) {
+	      evt.preventDefault();
+	    }
+	  };
+	
+	  formNameInput.oninput = function(evt) {
+	    evt.preventDefault();
+	    form.checkValidationName();
+	  };
+	
+	  formTextInput.oninput = function(evt) {
+	    evt.preventDefault();
+	    form.checkValidationText();
+	  };
+	
+	  for (var i = 0; i < formStars.length; i++) {
+	    formStars[i].onclick = function() {
+	      var numStars = this.control.value;
+	      form.checkValidationText(numStars);
+	    };
+	  }
 	
 	  return form;
 	})();
--- src/form.js
+++ src/form.js
@@ -3,6 +3,12 @@
 window.form = (function() {
   var formContainer = document.querySelector('.overlay-container');
   var formCloseButton = document.querySelector('.review-form-close');
+  var formSubmitButton = document.querySelector('.review-submit');
+  var formNameInput = document.querySelector('.review-form-field-name');
+  var formNameReminder = document.querySelector('.review-fields-name');
+  var formTextInput = document.querySelector('.review-form-field-text');
+  var formTextReminder = document.querySelector('.review-fields-text');
+  var formStars = document.querySelectorAll('.review-mark-label');
 
   var form = {
     onClose: null,
@@ -12,6 +18,7 @@ window.form = (function() {
      */
     open: function(cb) {
       formContainer.classList.remove('invisible');
+      this.checkValidation();
       cb();
     },
 
@@ -21,6 +28,37 @@ window.form = (function() {
       if (typeof this.onClose === 'function') {
         this.onClose();
       }
+    },
+
+    checkValidation: function() {
+      var isValidName = this.checkValidationName();
+      var isValidText = this.checkValidationText(this.getNumStars());
+      var isValid = isValidName && isValidText;
+      return isValid;
+    },
+
+    checkValidationName: function() {
+      var isValid = (formNameInput.value !== '');
+      this.setVisible(formNameReminder, !isValid);
+      return isValid;
+    },
+
+    checkValidationText: function(numStars) {
+      var isValid = (numStars >= 3 || formTextInput.value !== '');
+      this.setVisible(formTextReminder, !isValid);
+      return isValid;
+    },
+
+    setVisible: function(obj, value) {
+      if (value) {
+        obj.classList.remove('invisible');
+      } else {
+        obj.classList.add('invisible');
+      }
+    },
+
+    getNumStars: function() {
+      return document.querySelector('input[name="review-mark"]:checked').value;
     }
   };
 
@@ -29,6 +67,30 @@ window.form = (function() {
     evt.preventDefault();
     form.close();
   };
+
+  formSubmitButton.onclick = function(evt) {
+    var isValid = form.checkValidation();
+    if (!isValid) {
+      evt.preventDefault();
+    }
+  };
+
+  formNameInput.oninput = function(evt) {
+    evt.preventDefault();
+    form.checkValidationName();
+  };
+
+  formTextInput.oninput = function(evt) {
+    evt.preventDefault();
+    form.checkValidationText();
+  };
+
+  for (var i = 0; i < formStars.length; i++) {
+    formStars[i].onclick = function() {
+      var numStars = this.control.value;
+      form.checkValidationText(numStars);
+    };
+  }
 
   return form;
 })();
