1e527398d1ad7f93ae19c75927dee8848d9a2741
TaniaOsadchaia <osatanchik@gmail.com>
2016-08-23 22:48:28 +0300

    Мелкие правки

--- src/reviews.js
+++ src/reviews.js
@@ -8,48 +8,48 @@ var reviews = null;
 * @param {function} callback
 */
 function scriptRequest(src, callback) {
-	var success = false;
-	var id = 'cb_' + String(Math.random()).slice(-6);
+  var success = false;
+  var id = 'cb_' + String(Math.random()).slice(-6);
 
-	window[id] = function(data) {
-		success = true;
-		delete window[id];
-		callback(data);
-	};
+  window[id] = function(data) {
+    success = true;
+    delete window[id];
+    callback(data);
+  };
 
-	var checkCallback = function() {
-		var scriptElement = document.getElementById(id);
-		scriptElement.parentNode.removeChild(scriptElement);
+  var checkCallback = function() {
+    var scriptElement = document.getElementById(id);
+    scriptElement.parentNode.removeChild(scriptElement);
 
-   		if (!success) {
-   			delete window[id];
-    		callback(null); // error
-   		}
-  	};
+    if (!success) {
+      delete window[id];
+      callback(null); // error
+    }
+  };
 
-	var script = document.createElement('script');
-	script.onreadystatechange = function() {
-	    if (this.readyState == 'complete' || this.readyState == 'loaded') {
-			this.onreadystatechange = null;
-			setTimeout(checkCallback, 0);
-		}
-	}
-	script.onload = checkCallback;
-	script.onerror = checkCallback;
-	script.id = id;
-	script.src = src + '?callback=' + id;
-	document.body.appendChild(script);
+  var script = document.createElement('script');
+  script.onreadystatechange = function() {
+    if (this.readyState === 'complete' || this.readyState === 'loaded') {
+      this.onreadystatechange = null;
+      setTimeout(checkCallback, 0);
+    }
+  };
+  script.onload = checkCallback;
+  script.onerror = checkCallback;
+  script.id = id;
+  script.src = src + '?callback=' + id;
+  document.body.appendChild(script);
 }
 
 function onReviewsLoaded(data) {
-	reviews = data;
-	if (reviews instanceof Array) {
-		// success
-		console.log('Loaded ' + reviews.length + ' reviews');
-	} else {
-		// error
-		console.error('Can\'t load reviews');
-	}
+  reviews = data;
+  if (reviews instanceof Array) {
+    // success
+    console.log('Loaded ' + reviews.length + ' reviews');
+  } else {
+    // error
+    console.error('Can\'t load reviews');
+  }
 }
 
 scriptRequest('http://localhost:1506/api/reviews', onReviewsLoaded);
